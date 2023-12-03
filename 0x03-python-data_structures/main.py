afdb6840f7ff1d2b09b3f158ef73c6f223940935
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-12-03 15:35:29 +0900

    7

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
+add_tuple = __import__('7-add_tuple').add_tuple
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
 
-print(new_list)
-print(my_list)
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
