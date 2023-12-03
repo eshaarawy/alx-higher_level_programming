4f04b6e6f13060e356f661496fe3600d5f08cb28
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-12-03 15:28:43 +0900

    6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        if len(i) == 0:
+            print()
+        for j in range(len(i)):
+            print("{:d}".format(i[j]), end="\n"if j is len(i) - 1 else " ")
