737ab4d0cf12448bac0da399e72c86eadf4f8910
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-12-06 20:35:05 +0900

    6

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+print_sorted_dictionary(a_dictionary)
