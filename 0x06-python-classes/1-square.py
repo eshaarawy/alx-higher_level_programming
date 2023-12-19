f786f190f452d985cb6e2bf0b88ffa2f2162d38a
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-12-19 23:12:48 +0900

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        """Constructor.
+        
+        Args:
+            size: side length
+        """
+        self.__size = size
