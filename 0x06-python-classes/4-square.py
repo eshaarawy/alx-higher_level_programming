4ac3ff89160af40150a8827881bdf929df950fad
Mohamed Elshaarawy <elsharawy234@gmail.com>
2023-12-19 23:37:06 +0900

    4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieving size
+
+        Raises:
+            TypeError: If size is not integer.
+            ValueError: If size is less than 0.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        return self.__size
+
+    def area(self):
+        """Square Area
+
+        Returns:
+            Area of square.
+        """
+        return self.__size ** 2
+
