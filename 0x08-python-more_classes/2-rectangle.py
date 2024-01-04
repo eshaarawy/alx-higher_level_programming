f31913ed1d3a2d7284d2fd21e14cf969498c95b0
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-01-04 23:31:33 +0900

    3

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -46,10 +46,3 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return 0
         return (self.__height + self.__width) * 2
-
-    def __str__(self):
-        """returns printable string representation of rectangle"""
-        string = ""
-        if self.__width != 0 and self.__height != 0:
-                string += "\n".join("#" * self.__width for j in range(self.__height))
-        return string
