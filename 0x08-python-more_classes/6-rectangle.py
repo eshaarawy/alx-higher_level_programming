3bc4d078fba909162c4e24145b1aa1d2750c7be4
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-01-05 00:20:59 +0900

    6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,9 @@
 
 class Rectangle:
     """Represents a Rectangle """
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle."""
         self.width = width
