32bdb30a0ac37080862124d7295a71e7dff3b2dd
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-01-09 21:55:14 +0900

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""Task 7"""
-
+""" defining json """
 
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 arglist = list(sys.argv[1:])
 
 try:
