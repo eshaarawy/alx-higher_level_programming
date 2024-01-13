17658b9c5e9c09bcb27b2302a5f4b1cac040a62e
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-01-13 22:42:30 +0900

    1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base Module"""
+
+
+class Base:
+    """5ra"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
