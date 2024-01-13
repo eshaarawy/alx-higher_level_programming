cb9fe8cfc1184694960d71c1615b4ddac5fc19de
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-01-14 00:04:16 +0900

    12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,21 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """zz"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """5"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
