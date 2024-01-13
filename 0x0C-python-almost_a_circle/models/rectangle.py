baf9e782484d2742b0efd730de600d461b4cabf7
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-01-13 23:51:12 +0900

    8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,23 @@ class Rectangle(Base):
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        """5ra"""
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """5ra tany"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
