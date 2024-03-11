715222d5b24f32c6af6aec685a7dee78f235ab08
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-03-11 16:58:27 +0200

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (a) {
-  if (a === 0 || isNaN(a)) {
-    return 1;
+    if (a === 1 || isNaN(a)) {
+      return 1;
+    }
+    return a * factorial(a - 1);
   }
-  factorial(a - 1);
-}
-
-console.log(factorial(parseInt(process.argv[2])));
+  
+  console.log(factorial(parseInt(process.argv[2])));
