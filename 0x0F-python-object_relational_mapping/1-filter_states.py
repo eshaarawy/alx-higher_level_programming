83463da6c55ef18b44a0cb893dfe88defec2c885
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-03-16 22:03:02 +0200

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1].startswith('N'):
+            print(row)
     cur.close()
     db.close()
