f2a5552bc6ac1250ae18d32ff60ea28897a300e4
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-03-16 16:35:57 +0200

    16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,11 @@
--- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
--- Records are ordered by ascending genre name.
-SELECT g.`name`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON g.`id` = s.`genre_id`
+-- Lists all shows and genres linked to the show from the
+-- database hbtn_0d_tvshows.
+-- Records are ordered by ascending show title and genre name.
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+         LEFT JOIN `tv_show_genres` AS s
+	        ON t.`id` = s.`show_id`
 
-       INNER JOIN `tv_shows` AS t
-       ON t.`id` = s.`show_id`
-       WHERE t.`title` = "Dexter"
- ORDER BY g.`name`;
+       LEFT JOIN `tv_genres` AS g
+              ON s.`genre_id` = g.`id`
+	       ORDER BY t.`title`, g.`name`;
