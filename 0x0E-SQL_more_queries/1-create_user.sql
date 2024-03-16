ed43450e523f3663f79fdc65ba5156543a29b5d6
Mohamed Elshaarawy <elsharawy234@gmail.com>
2024-03-16 15:47:00 +0200

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- create a user user_0d_1
+-- with root privileges
+CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
